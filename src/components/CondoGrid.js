9b392e7008e1dc9b82e1639e35b827b2a6741784
sss1217 <siyun.liao@mail.mcgill.ca>
2022-04-16 18:24:00 -0400

    show condos listing

---
+++ src/components/CondoGrid.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { Table } from "semantic-ui-react"
+
+const DataTable = ({ condoData }) => {
+  return (
+    <Table celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Locality</Table.HeaderCell>
+          <Table.HeaderCell>neighbourhood_name</Table.HeaderCell>
+          <Table.HeaderCell>Home Type</Table.HeaderCell>
+          <Table.HeaderCell>Asking Price</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {condoData.map((c, index) => (
+          <Table.Row key={`condo_${index}`}>
+            <Table.Cell>{c.locality_name}</Table.Cell>
+            <Table.Cell>{c.neighbourhood_name}</Table.Cell>
+            <Table.Cell>{c.home_type}</Table.Cell>
+            <Table.Cell>{c.asking_price}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+
+      <Table.Footer>
+        <Table.Row>
+          <span> Bye </span>
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
+}
+
+const CondoGrid = () => {
+  const [condoData, setCondoData] = React.useState([])
+
+  const getCondoData = async () => {
+    const options = {
+      header: {
+        "User-Agent": "PostmanRuntime/7.26.8",
+      },
+    }
+    const res = await fetch("https://api.condos.ca/v1/listings", options)
+    const condos = await res.json()
+    console.log(condos)
+    setCondoData(condos)
+  }
+
+  React.useEffect(() => {
+    getCondoData()
+  }, [])
+
+  return (
+    <div>
+      <h3> Condo Grid </h3>
+      <DataTable condoData={condoData.data} />
+    </div>
+  )
+}
+
+export default CondoGrid
